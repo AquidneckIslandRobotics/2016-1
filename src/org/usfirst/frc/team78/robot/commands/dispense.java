b3eda71e0cd6ff7d9d627e288e3e8f98bad51530
FRC <FRC@DESKTOP-2RP803N>
2017-07-14 21:36:52 -0400

    quick

--- src/org/usfirst/frc/team78/robot/commands/dispense.java
+++ src/org/usfirst/frc/team78/robot/commands/dispense.java
@@ -25,7 +25,7 @@ public class dispense extends CommandGroup {
         // a CommandGroup containing them would require both the chassis and the
         // arm.
     	addSequential(new upperGate());
-    	addSequential(new doNothing(), 1);
+    	addSequential(new doNothing(), 0.5);
     	addSequential(new lowerGate());
     }
 }
